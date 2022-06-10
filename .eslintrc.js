dc6f609619314002cf4dcb3829364316baa84f6a
guihua.pgh <guihuapeng@126.com>
2022-06-10 17:51:00 +0800

    feat: init project

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: ['eslint-config-ali/typescript/node', 'prettier', 'prettier/@typescript-eslint'],
+  rules: {
+    'no-console': 'off',
+  },
+};
---
+++ README.md
@@ -0,0 +1,5 @@
+# Typescript 系列
+
+- [JavaScript](./docs/01.js.md)
+- [TypeScript](./docs/02.ts.md)
+- [TS 数据类型](./docs/03.data.type.md)
