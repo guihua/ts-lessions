78bf3e9698a73795868d41dad3d4b300946a6ff8
guihua.pgh <guihuapeng@126.com>
2022-06-15 14:13:07 +0800

    refactor: 配置 eslint 规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,9 @@ module.exports = {
   extends: ['eslint-config-ali/typescript/node', 'prettier', 'prettier/@typescript-eslint'],
   rules: {
     'no-console': 'off',
+    'no-irregular-whitespace': 'off',
+    'no-inferrable-types': 'off',
+    'no-redeclare': 'off',
+    'no-param-reassign': 'off',
   },
 };
